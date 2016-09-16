e04082339d936d53278edfc83e9afbaee25ad032
kirill <k.kobylyanskiy@gmail.com>
2016-09-16 05:34:41 +0300

    beta

--- LinearSystem.java
+++ LinearSystem.java
@@ -1,8 +1,56 @@
 public class LinearSystem {
+	
+	public double[][] matrix;
+	public double[][] x; 
+	public int strings;
 	LinearSystem(int n){
-		matrix = new double [n][n];
+		matrix = new double [n][n+1];
+		x = new double [n][1];
+		strings = n;
 	}
+	
+	public void algorithm(){
+		
+		// Прямой ход
+	
+		for(int i = 0; i < strings - 1; i++){
+			if(matrix[i][i] == 0.0) {
+				// Переставить уравнения
+				for(int j = i+1; j < strings-1; j++){
+					if(matrix[j][i] != 0){
+						// Переставить
+						for(int k = 0; k < strings+1; k++){
+							double t = matrix[i][i];
+							matrix[i][i] = matrix[j][i];
+							matrix[j][i] = t;
+						}
+					}
+				}
+			}
 
-	public double[][] matrix; 
+			double a1 = matrix[i][i];
+			for(int k = i+1; k < strings; k++){
+			double a2 = matrix[k][i];
+				for(int j = 0; j < strings + 1; j++){
+					matrix[k][j] += -matrix[i][j]*a2/a1;
+				}
+			}
+		}
+
+		// Обратный ход
+		
+		for(int i = strings-1; i >= 0; i--){
+			double s = 0;
+			for(int j = i + 1; j < strings; j++){
+				System.out.println(i+" "+j);
+				s += x[j][0]*matrix[i][j];
+			}
+			x[i][0] = (matrix[i][strings]-s)/matrix[i][i];	
+		}	
+
+		for(int i = 0; i < strings; i++){
+			System.out.print("X"+(i+1)+" = " +  x[i][0] + "\n");
+		}
+	}
 
 }
